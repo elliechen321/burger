3d3f96bfde10c813bf88bdeaebb90634caf676e1
Ellie Chen <ellie.cheny@gmail.com>
2018-03-09 01:21:55 -0600

    final commit

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Sylvester Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Marilyn Manson Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Joe Biden Burger', false);
